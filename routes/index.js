89c346e6c00440fd6d92746264ff1cc915f4ef45
fearthedear <linuskinzel@gmail.com>
2016-10-19 03:13:25 +0800

    setting up routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SITA prototype' });
 });
 
+router.get('/student', function (req, res) {
+  res.render('student');
+});
+
+router.get('/teacher', function (req, res) {
+  res.render('teacher');
+});
+
 module.exports = router;
