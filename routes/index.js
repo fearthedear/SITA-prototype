cd97660c47e1cf45a9fbe9170ba0d17b28b294e6
fearthedear <linuskinzel@gmail.com>
2016-10-19 23:42:17 +0800

    setting up student dashboard, naps

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SITA prototype' });
+  res.render('index' );
 });
 
 router.get('/student', function (req, res) {
